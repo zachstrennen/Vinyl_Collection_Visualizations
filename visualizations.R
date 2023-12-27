83c0b93f57996e9f7d39010fb40095154521bcfa
Zach Strennen <zachstrennen@zachs-mbp.myfiosgateway.com>
2023-12-27 13:14:20 -0500

    Updated visualizations with proper styling

--- visualizations.R
+++ visualizations.R
@@ -1,4 +1,6 @@
+# Load packages
 library(tidyverse)
+library(ggcorrplot)
 
 
 # Set working directory to wherever cloned repository is located locally
@@ -10,7 +12,10 @@ ds <- read.csv("data/music_collection.csv")
 # Histogram of album duration in minutes
 ggplot(ds, aes(x = Duration)) +
   geom_histogram(bins = 28, aes(y = ..density..), colour = "black", fill = "white") +
-  theme(axis.text.x = element_text(angle = -90, vjust = 0.5, hjust = 0), legend.position = "none") +
+  theme(
+    axis.text.x = element_text(angle = -90, vjust = 0.5, hjust = 0),
+    legend.position = "none"
+  ) +
   geom_vline(aes(xintercept = mean(Duration)), color = "blue", linetype = "dashed", size = 1) +
   geom_density(alpha = .2, fill = "#FF6666", outline.type = "full") +
   labs(
@@ -19,41 +24,47 @@ ggplot(ds, aes(x = Duration)) +
     title = "Duration Distribution"
   )
 
-# Histogram of duration split by type of vinyl
-ggplot(ds, aes(x = Duration)) +
-  geom_histogram(bins = 40, aes(y = ..density..), colour = "black", fill = "white") +
-  theme(axis.text.x = element_text(angle = -90, vjust = 0.5, hjust = 0), legend.position = "none") +
+# Density of album durations split by format
+ggplot(ds, aes(x = Duration, fill = Format)) +
+  geom_density(alpha = .2, outline.type = "full") +
   geom_vline(aes(xintercept = mean(Duration)), color = "blue", linetype = "dashed", size = 1) +
-  facet_wrap(~Format) +
-  geom_density(alpha = .2, fill = "#FF6666", outline.type = "full") +
+  theme(axis.text.x = element_text(angle = -90, vjust = 0.5, hjust = 0)) +
   labs(
     x = "Duration (minutes)",
-    y = "Density"
+    y = "Density",
+    title = "Duration Distributions by Type"
+  )
+
+# High frequency of artists
+artist_counts <- table(ds$Artist)
+simple_table <- as.data.frame(artist_counts)
+simple_table <- filter(simple_table, rank(desc(Freq)) <= 16)
+ggplot(simple_table, aes(x = reorder(Var1, Freq), y = Freq)) +
+  geom_bar(stat = "identity") +
+  coord_flip() +
+  labs(
+    x = "Artist",
+    y = "Frequency",
+    title = "Artists with three or more releases"
   )
 
 # Histogram of years it took to produce vinyl after albums release date
 ggplot(ds, aes(x = Vinyl.Release.Year - Album.Release.Year)) +
   geom_histogram(bins = 50, aes(y = ..density..), colour = "black", fill = "white") +
-  theme(axis.text.x = element_text(angle = -90, vjust = 0.5, hjust = 0), legend.position = "none") +
-  geom_vline(aes(xintercept = mean(Vinyl.Release.Year - Album.Release.Year)), color = "blue", linetype = "dashed", size = 1) +
+  theme(
+    axis.text.x = element_text(angle = -90, vjust = 0.5, hjust = 0),
+    legend.position = "none"
+  ) +
+  geom_vline(aes(xintercept = mean(Vinyl.Release.Year - Album.Release.Year)),
+    color = "blue", linetype = "dashed", size = 1
+  ) +
   geom_density(alpha = .2, fill = "#FF6666", outline.type = "full") +
   labs(
     x = "Years to Produce",
-    y = "Density"
+    y = "Density",
+    title = "Vinyl Release - Album Release"
   )
 
-
-# Highest frequency of artists
-table1 <- table(ds$Artist)
-simple <- as.data.frame(table1)
-simple <- filter(simple, rank(desc(Freq)) <= 16)
-ggplot(simple, aes(x = reorder(Var1, Freq), y = Freq)) +
-  geom_bar(stat = "identity") +
-  coord_flip() +
-  labs(
-    x = "Artist",
-    y = "Frequency"
-  )
 
 # Densities of album releases and vinyl releases
 ds1 <- transmute(ds, year = Album.Release.Year, Release = "Album Release")
@@ -64,53 +75,61 @@ ggplot(ds3, aes(x = year, fill = Release)) +
   theme(axis.text.x = element_text(angle = -90, vjust = 0.5, hjust = 0)) +
   labs(
     x = "Year",
-    y = "Density"
+    y = "Density",
+    title = "Album Release and Vinyl Release Distributions"
   )
 
-# Plot of vinyl release year vs. album release year
+# Plot of vinyl release year vs.nalbum release year
 ggplot(ds, aes(x = Album.Release.Year, y = Vinyl.Release.Year)) +
-  geom_point() +
-  theme(axis.text.x = element_text(angle = -90, vjust = 0.5, hjust = 0), legend.position = "none") +
-  geom_smooth(method = "loess", alpha = .15, fullrange = TRUE) +
+  geom_point(alpha = 0.5) +
+  theme(
+    axis.text.x = element_text(angle = -90, vjust = 0.5, hjust = 0),
+    legend.position = "none"
+  ) +
   labs(
     x = "Album Release Year",
-    y = "Vinyl Release Year"
-  )
-
-# Density of album durations split by format
-ggplot(ds, aes(x = Duration, fill = Format)) +
-  geom_density(alpha = .2, outline.type = "full") +
-  theme(axis.text.x = element_text(angle = -90, vjust = 0.5, hjust = 0)) +
-  labs(
-    x = "Duration (minutes)",
-    y = "Density"
+    y = "Vinyl Release Year",
+    title = "Vinyl Release Year vs. Album Release Year"
   )
 
 # Density of album release year
 ggplot(ds, aes(x = Album.Release.Year, fill = "red")) +
   geom_density(alpha = .2, outline.type = "full") +
+  geom_vline(aes(xintercept = mean(Album.Release.Year)),
+    color = "blue", linetype = "dashed", size = 1
+  ) +
   theme(axis.text.x = element_text(angle = -90, vjust = 0.5, hjust = 0)) +
   labs(
     x = "Album Release Year",
-    y = "Density"
-  )
+    y = "Density",
+    title = "Distribution of Album Release Year"
+  ) +
+  guides(fill = FALSE)
 
 # Density of album release year by format
 ggplot(ds, aes(x = Album.Release.Year, fill = Format)) +
   geom_density(alpha = .2, outline.type = "full") +
+  geom_vline(aes(xintercept = mean(Album.Release.Year)),
+    color = "blue", linetype = "dashed", size = 1
+  ) +
   theme(axis.text.x = element_text(angle = -90, vjust = 0.5, hjust = 0)) +
   labs(
     x = "Album Release Year",
-    y = "Density"
+    y = "Density",
+    title = "Distribution of Album Release Year by Type"
   )
 
-# density of album release year by format
+# Density of album release year by format
 ggplot(ds, aes(x = Vinyl.Release.Year, fill = Format)) +
   geom_density(alpha = .2, outline.type = "full") +
+  geom_vline(aes(xintercept = mean(Vinyl.Release.Year)),
+    color = "blue", linetype = "dashed", size = 1
+  ) +
   theme(axis.text.x = element_text(angle = -90, vjust = 0.5, hjust = 0)) +
   labs(
     x = "Vinyl Release Year",
-    y = "Density"
+    y = "Density",
+    title = "Distribution of Vinyl Release Year by Type"
   )
 
 # Histogram of track counts
@@ -121,16 +140,21 @@ ggplot(ds, aes(x = Track.Count)) +
   geom_density(alpha = .2, fill = "#FF6666", outline.type = "full") +
   labs(
     x = "Track Count",
-    y = "Density"
+    y = "Density",
+    title = "Distribution of Track Count"
   )
 
 # Density of track counts by format
 ggplot(ds, aes(x = Track.Count, fill = Format)) +
   geom_density(alpha = .2, outline.type = "full") +
+  geom_vline(aes(xintercept = mean(Track.Count)),
+    color = "blue", linetype = "dashed", size = 1
+  ) +
   theme(axis.text.x = element_text(angle = -90, vjust = 0.5, hjust = 0)) +
   labs(
     x = "Track Count",
-    y = "Density"
+    y = "Density",
+    title = "Distribution of Track Count by Type"
   )
 
 # Plot of track count vs. duration colored by format
@@ -139,10 +163,12 @@ ggplot(ds, aes(x = Duration, y = Track.Count, color = Format)) +
   geom_smooth(method = "loess", alpha = .15, fullrange = TRUE, color = "black") +
   labs(
     x = "Duration (minutes)",
-    y = "Track Count"
+    y = "Track Count",
+    title = "Track Count vs. Duration"
   )
 
-# Frequencies of certain features
-table(ds$Format)
-table(ds$Vinyl.Release.Year)
-table(ds$Album.Release.Year)
+# Correlation matrix of numeric variables
+numeric_cols <- c("Duration", "Album.Release.Year", "Vinyl.Release.Year", "Track.Count")
+ds[numeric_cols] <- lapply(ds[numeric_cols], as.numeric)
+corr_matrix <- cor(ds[numeric_cols])
+ggcorrplot(corr_matrix)
